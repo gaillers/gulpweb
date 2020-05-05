f9c909374dc719e3f5d8322f472380edb7a4c41f
gaillers <gailers@mail.ru>
2020-05-05 16:55:38 +0300

    first release

---
+++ build/js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var name = 'Alex';
+var age = 24;
+var sex = 'male';
+var city = 'odessa';
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+let name = 'Alex'
+let age = 24;
+let sex = 'male';
+let city = 'odessa';
