f9d04ad71d232e2bf51d184f9bc783bd66d09cd4
Alexey Horodesky <gailers@mail.ru>
2020-08-29 19:57:08 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ const gulp = require('gulp'),
  rename = require('gulp-rename'),
  ttf2woff = require('gulp-ttf2woff'),
  ttf2woff2 = require('gulp-ttf2woff2');
-
+ htmlmin = require('gulp-htmlmin');
 
 // порядок подкл css
 const styleFiles = [
@@ -46,6 +46,17 @@ gulp.task('normalize', function () {
     .pipe(gulp.dest('./build/css'))
 });
 
+// выгрузка html файла 
+gulp.task('minify', () => {
+    return gulp.src('./*.html')
+      .pipe(htmlmin({ collapseWhitespace: false }))
+      .pipe(gulp.dest('build'));
+
+});
+gulp.task('pages', () => {
+    return gulp.src('./src/pages/*.html')
+      .pipe(gulp.dest('build/pages/'));
+});
 // Стили
 gulp.task('styles', function () {
     return gulp.src(styleFiles)
@@ -122,7 +133,11 @@ gulp.task('img-compress', () => {
     .pipe(gulp.dest('./build/images'))
 }); 
 
-// Шрифты
+// Шрифт
+gulp.task('ttf', () => {
+    return gulp.src('./src/fonts/*.ttf')
+      .pipe(gulp.dest('build/fonts/'));
+});
 gulp.task('fonts', function () {
     return gulp.src(['./src/fonts/*.ttf'])
        .pipe(ttf2woff())
@@ -134,26 +149,29 @@ gulp.task('font', function () {
       .pipe(gulp.dest('./build/fonts'));
 });
 
-
 // Общие
 gulp.task('del', function () {
     return del(['build/*'])
 });
+
 gulp.task('watch', function (done) {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
+    gulp.watch('./src/pages/*.html', gulp.series('pages'));
+    gulp.watch('./*html', gulp.series('minify'));
     gulp.watch('./src/styles/**/*.css', gulp.series('styles'));
     gulp.watch('./src/styles/**/*.scss', gulp.series('styles'));
     gulp.watch('./src/styles/**/*.sass', gulp.series('styles'));
     gulp.watch('./src/js/**/*.js', gulp.series('scripts'));
     gulp.watch('./src/images/**/*.{jpg,png,svg,gif,ico,webp}', gulp.series('img-compress'));
+    gulp.watch('./src/fonts/*.ttf', gulp.series('ttf'));
     gulp.watch('./src/fonts/*.ttf', gulp.series('font'));
     gulp.watch('./src/fonts/*.ttf', gulp.series('fonts'));
     gulp.watch("./*.html").on('change', browserSync.reload);
     done()
 });
 
-gulp.task('default', gulp.series('del', gulp.parallel('normalize', 'styles', 'scripts', 'img-compress','font','fonts'), 'watch'));
+gulp.task('default', gulp.series('del', gulp.parallel('pages','minify','normalize','styles', 'scripts', 'img-compress','font','fonts','ttf'), 'watch'));
